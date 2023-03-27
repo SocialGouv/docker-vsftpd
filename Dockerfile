3f8ed4ba49c46ead72c477b299a7a9efb1b7b3c8
devthejo <jo@surikat.pro>
2023-03-27 19:49:15 +0200

    chore: wip

--- Dockerfile
+++ Dockerfile
@@ -11,27 +11,20 @@ RUN apt-get update && \
 
 RUN groupadd -g 1001 vsftpd && useradd -rm -d /home/vsftpd -s /bin/bash -g vsftpd -G sudo -u 1001 vsftpd
 
-ENV FTP_USER **String**
-ENV FTP_PASS **String**
-ENV PASV_ADDRESS **IPv4**
+ENV FTP_USER admin
+ENV FTP_PASS admin
 ENV PASV_ADDR_RESOLVE NO
 ENV PASV_ENABLE YES
 ENV PASV_MIN_PORT 21100
 ENV PASV_MAX_PORT 21110
-ENV XFERLOG_STD_FORMAT NO
-ENV LOG_STDOUT **Boolean**
 ENV FILE_OPEN_MODE 0666
 ENV LOCAL_UMASK 077
-ENV REVERSE_LOOKUP_ENABLE YES
 ENV PASV_PROMISCUOUS NO
 ENV PORT_PROMISCUOUS NO
 
-COPY vsftpd.conf /etc/vsftpd/
-COPY vsftpd_virtual /etc/pam.d/
-COPY run-vsftpd.sh /usr/sbin/
-
-RUN chmod +x /usr/sbin/run-vsftpd.sh
 RUN mkdir -p /home/vsftpd/
+RUN mkdir -p /etc/vsftpd.d/ \
+	&& chown /etc/vsftpd.d
 RUN chown -R vsftpd:vsftpd /home/vsftpd/
 RUN chown -R vsftpd:vsftpd /etc/vsftpd/
 RUN touch /etc/vsftpd.key \
@@ -39,13 +32,11 @@ RUN touch /etc/vsftpd.key \
 	&& chown vsftpd:vsftpd /etc/vsftpd.key \
 	&& chown vsftpd:vsftpd /etc/vsftpd.pem
 
-WORKDIR /home/vsftpd
-
-VOLUME /home/vsftpd
-VOLUME /var/log/vsftpd
-
-EXPOSE 20 21
-
+USER 1001
 CMD ["/usr/sbin/run-vsftpd.sh"]
 
-USER 1001
+COPY vsftpd.conf /etc/vsftpd/
+COPY vsftpd_virtual /etc/pam.d/
+COPY run-vsftpd.sh /usr/sbin/
+
+RUN chmod +x /usr/sbin/run-vsftpd.sh
--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -2,42 +2,22 @@
 
 set -e
 
-export RANDFILE=/home/vsftpd/.rnd
-openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/vsftpd.key -out /etc/vsftpd.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
+openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/vsftpd.key -out /etc/vsftpd.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress=" 2>/dev/null
 
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
-
-# If no env var for FTP_PASS has been specified, use 'admin':
-if [ "$FTP_PASS" = "**String**" ]; then
-    export FTP_PASS="admin"
-fi
-
-# Create home dir and update vsftpd user db:
 mkdir -p "/home/vsftpd/${FTP_USER}"
-
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
+echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd.d/env.conf
+echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd.d/env.conf
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd.d/env.conf
+echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd.d/env.conf
+echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd.d/env.conf
+echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd.d/env.conf
+echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd.d/env.conf
+echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd.d/env.conf
 
-if [ -f /etc/vsftpd.source/vsftpd.conf ]; then
-	cp /etc/vsftpd.source/vsftpd.conf /etc/vsftpd/vsftpd.conf
-fi
+cat /etc/vsftpd.d/* >> /etc/vsftpd.d/env.conf
 
-if [ "$PASV_ADDRESS" != "**IPv4**" ]; then
-	echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-fi
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
-echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-
-# Run vsftpd:
-exec vsftpd /etc/vsftpd/vsftpd.conf
+echo "starting vsftpd server"
+exec vsftpd -xferlog_enable=YES -vsftpd_log_file=$(tty) /etc/vsftpd/vsftpd.conf
