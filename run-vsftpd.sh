fb50f1add381dd74cc833348770be370a766f368
devthejo <jo@surikat.pro>
2023-03-27 20:01:42 +0200

    chore: wip

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -8,16 +8,18 @@ mkdir -p "/home/vsftpd/${FTP_USER}"
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd.d/env.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd.d/env.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd.d/env.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd.d/env.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd.d/env.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd.d/env.conf
-echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd.d/env.conf
-echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd.d/env.conf
+if [ -d /etc/vsftpd.d ]; then
+	cat /etc/vsftpd.d/* >> /etc/vsftpd/vsftpd.conf
+fi
 
-cat /etc/vsftpd.d/* >> /etc/vsftpd/vsftpd.conf
+echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
+echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
+echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
 
 echo "starting vsftpd server"
 exec vsftpd /etc/vsftpd/vsftpd.conf
